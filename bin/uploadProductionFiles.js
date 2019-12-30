8387db8f6ede6d5e9cb5243dcebb1444899f6579
eaTong <eatongchou@gmail.com>
2019-12-30 14:25:11 +0800

    team manager

--- bin/uploadProductionFiles.js
+++ bin/uploadProductionFiles.js
@@ -1,15 +1,17 @@
 const client = require('scp2');
 const fs = require('fs');
+const config = require('../config/server.config.production');
+
 client.addListener('transfer', (a, b, c) => {
   console.log(a, b, c);
 });
-
-client.scp('build/', {
+const defaultOptions = {
   host: '118.24.75.69',
   username: 'ubuntu',
-  privateKey: fs.readFileSync(fs.existsSync('C:\\certificate') ? 'C:\\certificate\\eaTong' : '/Users/liuyajun/certifcate/eaTong'),
+  privateKey: fs.readFileSync('/Users/eatong/certificate/eaTong_pem'),
   passphrase: '',
   path: '/home/ubuntu/iget-build'
-}, function (err, ...args) {
+};
+client.scp('build/', config.ssh || defaultOptions, function (err, ...args) {
   console.log(err, ...args);
 });
